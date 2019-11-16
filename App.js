3b5781184643a4b4eb42a3d0f29ea4c658fe31e2
Vitao18 <victoronline9@gmail.com>
2019-11-16 16:29:33 -0300

    done commit

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
+      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
+      <Text>Current Count: {counter}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;
